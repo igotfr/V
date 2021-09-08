822abf5b82a5fb81638d1950d49da87c3b25d010
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-08 00:57:18 -0300

    Update bubble.v

--- algorithms/sort/bubble.v
+++ algorithms/sort/bubble.v
@@ -1,13 +1,13 @@
 fn bubble_sort_mutation<T>(mut array_to_sort []T) {
-  //mut aux := 0
+  //mut tmp := 0
 
   for i in 0..array_to_sort.len {
     for j in i + 1..array_to_sort.len {
       if array_to_sort[i] > array_to_sort[j] {
         array_to_sort[i], array_to_sort[j] = array_to_sort[j], array_to_sort[i]
-        /*aux = pVetor[i]
-        pVetor[i] = pVetor[j]
-        pVetor[j] = aux*/
+        /*tmp = array_to_sort[i]
+        array_to_sort[i] = array_to_sort[j]
+        array_to_sort[j] = tmp*/
       }
     }
   }
@@ -15,15 +15,15 @@ fn bubble_sort_mutation<T>(mut array_to_sort []T) {
 
 fn bubble_sort<T>(array_to_sort []T) []T {
   mut array_to_sort_clone := array_to_sort.clone()
-  //mut aux := 0
+  //mut tmp := 0
 
   for i in 0..array_to_sort_clone.len {
     for j in i + 1..array_to_sort_clone.len {
       if array_to_sort_clone[i] > array_to_sort_clone[j] {
         array_to_sort_clone[i], array_to_sort_clone[j] = array_to_sort_clone[j], array_to_sort_clone[i]
-        /*aux = pVetor[i]
-        pVetor[i] = pVetor[j]
-        pVetor[j] = aux*/
+        /*tmp = array_to_sort[i]
+        array_to_sort[i] = array_to_sort[j]
+        array_to_sort[j] = tmp*/
       }
     }
   }
