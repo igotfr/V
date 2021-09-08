ed620ea1937e263929388c4fab7d3b59a7786d31
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-08 00:01:34 -0300

    Update bubble.v

--- algorithms/sort/bubble.v
+++ algorithms/sort/bubble.v
@@ -4,7 +4,7 @@ fn bubble_sort_mutation<T>(mut array_to_sort []T) {
   for i in 0..array_to_sort.len {
     for j in i + 1..array_to_sort.len {
       if array_to_sort[i] > array_to_sort[j] {
-				array_to_sort[i], array_to_sort[j] = array_to_sort[j], array_to_sort[i]
+        array_to_sort[i], array_to_sort[j] = array_to_sort[j], array_to_sort[i]
         /*aux = pVetor[i]
         pVetor[i] = pVetor[j]
         pVetor[j] = aux*/
@@ -20,7 +20,7 @@ fn bubble_sort<T>(array_to_sort []T) []T {
   for i in 0..array_to_sort_clone.len {
     for j in i + 1..array_to_sort_clone.len {
       if array_to_sort_clone[i] > array_to_sort_clone[j] {
-				array_to_sort_clone[i], array_to_sort_clone[j] = array_to_sort_clone[j], array_to_sort_clone[i]
+        array_to_sort_clone[i], array_to_sort_clone[j] = array_to_sort_clone[j], array_to_sort_clone[i]
         /*aux = pVetor[i]
         pVetor[i] = pVetor[j]
         pVetor[j] = aux*/
