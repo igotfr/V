85e15d697b3a33d9a88335a4fd31e28c2b1fb31f
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-15 20:16:28 -0300

    Update bubble.v

--- algorithms/sort/bubble.v
+++ algorithms/sort/bubble.v
@@ -1,4 +1,4 @@
-fn bubble_sort_mutation<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
+fn bubble_sort_body<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
   array_to_sort_len := array_to_sort.len
 
   for i in 0..array_to_sort_len {
@@ -13,20 +13,14 @@ fn bubble_sort_mutation<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
   }
 }
 
+fn bubble_sort_mutation<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
+  bubble_sort_body<T>(mut array_to_sort, compare)
+}
+
 fn bubble_sort<T>(array_to_sort []T, compare fn (a T, b T) bool) []T {
   mut array_to_sort_clone := array_to_sort.clone()
-  array_to_sort_clone_len := array_to_sort_clone.len
 
-  for i in 0..array_to_sort_clone_len {
-    for j in i + 1..array_to_sort_clone_len {
-      if compare(array_to_sort_clone[i], array_to_sort_clone[j]) {
-        array_to_sort_clone[i], array_to_sort_clone[j] = array_to_sort_clone[j], array_to_sort_clone[i]
-        /*tmp := array_to_sort_clone[i]
-        array_to_sort_clone[i] = array_to_sort_clone[j]
-        array_to_sort_clone[j] = tmp*/
-      }
-    }
-  }
+  bubble_sort_body<T>(mut array_to_sort_clone, compare)
 
   return array_to_sort_clone
 }
