d2e508326b7d154e4e28aabfb9c3b104d0173461
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-15 16:42:55 -0300

    Update selection.v

--- algorithms/sort/selection.v
+++ algorithms/sort/selection.v
@@ -2,19 +2,20 @@ fn selection_sort_mutation<T>(mut array_to_sort []T) {
   //mut tmp := 0
 
   for i in 0..array_to_sort.len {
-    mut loi := i
+    // index of lowest
+    mut ilo := i
 
     for j in i + 1..array_to_sort.len {
-      if array_to_sort[loi] > array_to_sort[j] {
-        loi = j
+      if array_to_sort[ilo] > array_to_sort[j] {
+        ilo = j
       }
     }
 
     //if i != loi {
-      array_to_sort[i], array_to_sort[loi] = array_to_sort[loi], array_to_sort[i]
+      array_to_sort[i], array_to_sort[ilo] = array_to_sort[ilo], array_to_sort[i]
       /*tmp = array_to_sort[i]
-      array_to_sort[i] = array_to_sort[j]
-      array_to_sort[j] = tmp*/
+      array_to_sort[i] = array_to_sort[ilo]
+      array_to_sort[ilo] = tmp*/
     //}
   }
 }
@@ -24,19 +25,20 @@ fn selection_sort<T>(array_to_sort []T) []T {
   //mut tmp := 0
 
   for i in 0..array_to_sort_clone.len {
-    mut loi := i
+    // index of lowest
+    mut ilo := i
 
     for j in i + 1..array_to_sort_clone.len {
-      if array_to_sort_clone[loi] > array_to_sort_clone[j] {
-        loi = j
+      if array_to_sort_clone[ilo] > array_to_sort_clone[j] {
+        ilo = j
       }
     }
 
     //if i != loi {
-      array_to_sort_clone[i], array_to_sort_clone[loi] = array_to_sort_clone[loi], array_to_sort_clone[i]
+      array_to_sort_clone[i], array_to_sort_clone[ilo] = array_to_sort_clone[ilo], array_to_sort_clone[i]
       /*tmp = array_to_sort[i]
-      array_to_sort[i] = array_to_sort[j]
-      array_to_sort[j] = tmp*/
+      array_to_sort[i] = array_to_sort[ilo]
+      array_to_sort[ilo] = tmp*/
     //}
   }
 
