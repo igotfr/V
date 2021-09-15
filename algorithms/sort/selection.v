bbbcb5c889038b6526d419f4aed1555e94b374c2
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-15 19:16:10 -0300

    Update selection.v

--- algorithms/sort/selection.v
+++ algorithms/sort/selection.v
@@ -1,9 +1,11 @@
 fn selection_sort_mutation<T>(mut array_to_sort []T) {
-  for i in 0..array_to_sort.len {
+  array_to_sort_len := array_to_sort.len
+
+  for i in 0..array_to_sort_len {
     // index of lowest
     mut ilo := i
 
-    for j in i + 1..array_to_sort.len {
+    for j in i + 1..array_to_sort_len {
       if array_to_sort[ilo] > array_to_sort[j] {
         ilo = j
       }
@@ -20,12 +22,13 @@ fn selection_sort_mutation<T>(mut array_to_sort []T) {
 
 fn selection_sort<T>(array_to_sort []T) []T {
   mut array_to_sort_clone := array_to_sort.clone()
+  array_to_sort_clone_len := array_to_sort_clone.len
 
-  for i in 0..array_to_sort_clone.len {
+  for i in 0..array_to_sort_clone_len {
     // index of lowest
     mut ilo := i
 
-    for j in i + 1..array_to_sort_clone.len {
+    for j in i + 1..array_to_sort_clone_len {
       if array_to_sort_clone[ilo] > array_to_sort_clone[j] {
         ilo = j
       }
