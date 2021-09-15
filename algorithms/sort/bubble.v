30a53ab4f7f45dba6b0766afd7d2b409cd4c17e8
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-15 18:31:57 -0300

    Update bubble.v

--- algorithms/sort/bubble.v
+++ algorithms/sort/bubble.v
@@ -1,11 +1,9 @@
 fn bubble_sort_mutation<T>(mut array_to_sort []T) {
-  //mut tmp := 0
-
   for i in 0..array_to_sort.len {
     for j in i + 1..array_to_sort.len {
       if array_to_sort[i] > array_to_sort[j] {
         array_to_sort[i], array_to_sort[j] = array_to_sort[j], array_to_sort[i]
-        /*tmp = array_to_sort[i]
+        /*tmp := array_to_sort[i]
         array_to_sort[i] = array_to_sort[j]
         array_to_sort[j] = tmp*/
       }
@@ -15,13 +13,12 @@ fn bubble_sort_mutation<T>(mut array_to_sort []T) {
 
 fn bubble_sort<T>(array_to_sort []T) []T {
   mut array_to_sort_clone := array_to_sort.clone()
-  //mut tmp := 0
 
   for i in 0..array_to_sort_clone.len {
     for j in i + 1..array_to_sort_clone.len {
       if array_to_sort_clone[i] > array_to_sort_clone[j] {
         array_to_sort_clone[i], array_to_sort_clone[j] = array_to_sort_clone[j], array_to_sort_clone[i]
-        /*tmp = array_to_sort_clone[i]
+        /*tmp := array_to_sort_clone[i]
         array_to_sort_clone[i] = array_to_sort_clone[j]
         array_to_sort_clone[j] = tmp*/
       }
