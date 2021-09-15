349923e52835adc644c11da63e1e97159b839bd1
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-15 19:55:17 -0300

    Update bubble.v

--- algorithms/sort/bubble.v
+++ algorithms/sort/bubble.v
@@ -1,9 +1,9 @@
-fn bubble_sort_mutation<T>(mut array_to_sort []T) {
+fn bubble_sort_mutation<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
   array_to_sort_len := array_to_sort.len
 
   for i in 0..array_to_sort_len {
     for j in i + 1..array_to_sort_len {
-      if array_to_sort[i] > array_to_sort[j] {
+      if compare(array_to_sort[i], array_to_sort[j]) {
         array_to_sort[i], array_to_sort[j] = array_to_sort[j], array_to_sort[i]
         /*tmp := array_to_sort[i]
         array_to_sort[i] = array_to_sort[j]
@@ -13,13 +13,13 @@ fn bubble_sort_mutation<T>(mut array_to_sort []T) {
   }
 }
 
-fn bubble_sort<T>(array_to_sort []T) []T {
+fn bubble_sort<T>(array_to_sort []T, compare fn (a T, b T) bool) []T {
   mut array_to_sort_clone := array_to_sort.clone()
   array_to_sort_clone_len := array_to_sort_clone.len
 
   for i in 0..array_to_sort_clone_len {
     for j in i + 1..array_to_sort_clone_len {
-      if array_to_sort_clone[i] > array_to_sort_clone[j] {
+      if compare(array_to_sort_clone[i], array_to_sort_clone[j]) {
         array_to_sort_clone[i], array_to_sort_clone[j] = array_to_sort_clone[j], array_to_sort_clone[i]
         /*tmp := array_to_sort_clone[i]
         array_to_sort_clone[i] = array_to_sort_clone[j]
