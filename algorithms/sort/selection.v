e69157ada362a889c45418386a55326bf2c8283a
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-15 20:24:25 -0300

    Update selection.v

--- algorithms/sort/selection.v
+++ algorithms/sort/selection.v
@@ -1,4 +1,4 @@
-fn selection_sort_mutation<T>(mut array_to_sort []T) {
+fn selection_sort_body<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
   array_to_sort_len := array_to_sort.len
 
   for i in 0..array_to_sort_len {
@@ -6,12 +6,12 @@ fn selection_sort_mutation<T>(mut array_to_sort []T) {
     mut ilo := i
 
     for j in i + 1..array_to_sort_len {
-      if array_to_sort[ilo] > array_to_sort[j] {
+      if compare(array_to_sort[ilo], array_to_sort[j]) {
         ilo = j
       }
     }
 
-    //if i != loi {
+    //if i != ilo {
       array_to_sort[i], array_to_sort[ilo] = array_to_sort[ilo], array_to_sort[i]
       /*tmp := array_to_sort[i]
       array_to_sort[i] = array_to_sort[ilo]
@@ -20,27 +20,14 @@ fn selection_sort_mutation<T>(mut array_to_sort []T) {
   }
 }
 
-fn selection_sort<T>(array_to_sort []T) []T {
+fn selection_sort_mutation<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
+  selection_sort_body<T>(mut array_to_sort, compare)
+}
+
+fn selection_sort<T>(array_to_sort []T, compare fn (a T, b T) bool) []T {
   mut array_to_sort_clone := array_to_sort.clone()
-  array_to_sort_clone_len := array_to_sort_clone.len
 
-  for i in 0..array_to_sort_clone_len {
-    // index of lowest
-    mut ilo := i
-
-    for j in i + 1..array_to_sort_clone_len {
-      if array_to_sort_clone[ilo] > array_to_sort_clone[j] {
-        ilo = j
-      }
-    }
-
-    //if i != loi {
-      array_to_sort_clone[i], array_to_sort_clone[ilo] = array_to_sort_clone[ilo], array_to_sort_clone[i]
-      /*tmp := array_to_sort_clone[i]
-      array_to_sort_clone[i] = array_to_sort_clone[ilo]
-      array_to_sort_clone[ilo] = tmp*/
-    //}
-  }
+  selection_sort_body<T>(mut array_to_sort_clone, compare)
 
   return array_to_sort_clone
 }
