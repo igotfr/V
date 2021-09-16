dcaf6ee82fefad8f5f89771e0eb1d772850d2109
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-16 02:48:10 -0300

    Update bubble.v

--- algorithms/sort/bubble.v
+++ algorithms/sort/bubble.v
@@ -29,7 +29,9 @@ fn bubble_sort_loop<T>(array_to_sort []T, compare fn (a T, b T) bool) []T {
 
 // Recursion
 
-fn bubble_sort_body_recursion<T>(mut array_to_sort []T, array_to_sort_len int, compare fn (a T, b T) bool) {
+fn bubble_sort_body_recursion<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
+  array_to_sort_len := array_to_sort.len
+
   if array_to_sort_len <= 1 { return }
 
   for i in 0..array_to_sort_len - 1 {
@@ -41,17 +43,17 @@ fn bubble_sort_body_recursion<T>(mut array_to_sort []T, array_to_sort_len int, c
     }
   }
 
-  bubble_sort_body_recursion<T>(mut array_to_sort, array_to_sort_len - 1, compare)
+  bubble_sort_body_recursion<T>(mut array_to_sort[0..array_to_sort_len - 1], compare)
 }
 
 fn bubble_sort_recursion_mutation<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
-  bubble_sort_body_recursion<T>(mut array_to_sort, array_to_sort.len, compare)
+  bubble_sort_body_recursion<T>(mut array_to_sort, compare)
 }
 
 fn bubble_sort_recursion<T>(array_to_sort []T, compare fn (a T, b T) bool) []T {
   mut array_to_sort_clone := array_to_sort.clone()
 
-  bubble_sort_body_recursion<T>(mut array_to_sort_clone, array_to_sort.len, compare)
+  bubble_sort_body_recursion<T>(mut array_to_sort_clone, compare)
 
   return array_to_sort_clone
 }
