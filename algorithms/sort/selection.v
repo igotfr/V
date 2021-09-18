22c59b791165f54332a2132eb5f43931c27c1f54
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-09-18 20:48:05 -0300

    Update selection.v

--- algorithms/sort/selection.v
+++ algorithms/sort/selection.v
@@ -1,4 +1,6 @@
-fn selection_sort_body<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
+// Loop
+
+fn selection_sort_loop<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
   array_to_sort_len := array_to_sort.len
 
   for i in 0..array_to_sort_len {
@@ -20,14 +22,69 @@ fn selection_sort_body<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
   }
 }
 
-fn selection_sort_mutation<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
-  selection_sort_body<T>(mut array_to_sort, compare)
-}
 
-fn selection_sort<T>(array_to_sort []T, compare fn (a T, b T) bool) []T {
+fn selection_sort_loop_clone<T>(array_to_sort []T, compare fn (a T, b T) bool) []T {
   mut array_to_sort_clone := array_to_sort.clone()
 
-  selection_sort_body<T>(mut array_to_sort_clone, compare)
+  selection_sort_loop<T>(mut array_to_sort_clone, compare)
 
   return array_to_sort_clone
+}
+
+// Recursion
+
+fn selection_sort_recursion<T>(mut array_to_sort []T, compare fn (a T, b T) bool) {
+  array_to_sort_len := array_to_sort.len
+
+  //if array_to_sort_len <= 1 { return }
+
+  // index of lowest
+  i := 0
+  mut ilo := i
+
+  for j in i + 1..array_to_sort_len {
+    if compare(array_to_sort[ilo], array_to_sort[j]) {
+      ilo = j
+    }
+  }
+
+  //if i != ilo {
+    array_to_sort[i], array_to_sort[ilo] = array_to_sort[ilo], array_to_sort[i]
+    /*tmp := array_to_sort[i]
+    array_to_sort[i] = array_to_sort[ilo]
+    array_to_sort[ilo] = tmp*/
+  //}
+
+  if i + 1 < array_to_sort_len {
+    selection_sort_recursion<T>(mut array_to_sort[i + 1..], compare)
+  }
+}
+
+fn selection_sort_recursion_clone<T>(array_to_sort []T, compare fn (a T, b T) bool) []T {
+  mut array_to_sort_clone := array_to_sort.clone()
+
+  selection_sort_recursion<T>(mut array_to_sort_clone, compare)
+
+  return array_to_sort_clone
+}
+
+// Selection Sort
+
+enum LoopRec {
+  loop
+  recursion
+}
+
+fn selection_sort<T>(mut array_to_sort []T, compare fn (a T, b T) bool, loop_rec LoopRec) {
+  match loop_rec {
+    .loop { selection_sort_loop<T>(mut array_to_sort, compare) }
+    .recursion { selection_sort_recursion<T>(mut array_to_sort, compare) }
+  }
+}
+
+fn selection_sort_clone<T>(array_to_sort []T, compare fn (a T, b T) bool, loop_rec LoopRec) []T {
+  return match loop_rec {
+    .loop { selection_sort_loop_clone<T>(array_to_sort, compare) }
+    .recursion { selection_sort_recursion_clone<T>(array_to_sort, compare) }
+  }
 }
