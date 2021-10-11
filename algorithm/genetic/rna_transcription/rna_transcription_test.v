0e5e9b4c9d4db7d6db3e336b3e0b6bb035767ec4
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-11 01:00:58 -0300

    Update rna_transcription_test.v

--- algorithm/genetic/rna_transcription/rna_transcription_test.v
+++ algorithm/genetic/rna_transcription/rna_transcription_test.v
@@ -1,23 +1,23 @@
 module rna_transcription
  
 fn test_transcribes_guanine_to_cytosine() {
-  assert to_rna('G') or { err.msg } == 'C'
+  assert to_rna('G') or { 'Invalid DNA: ' + err.msg } == 'C'
 }
 
 fn test_transcribes_cytosine_to_guanine() {
-  assert to_rna('C') or { err.msg } == 'G'
+  assert to_rna('C') or { 'Invalid DNA: ' + err.msg } == 'G'
 }
 
 fn test_transcribes_thymidine_to_adenine() {
-  assert to_rna('T') or { err.msg } == 'A'
+  assert to_rna('T') or { 'Invalid DNA: ' + err.msg } == 'A'
 }
 
 fn test_transcribes_adenine_to_uracil() {
-  assert to_rna('A') or { err.msg } == 'U'
+  assert to_rna('A') or { 'Invalid DNA: ' + err.msg } == 'U'
 }
 
 fn test_it_transcribes_all_dna_nucleotides_to_rna_equivalents() {
-  assert to_rna('ACGTGGTCTTAA') or { err.msg } == 'UGCACCAGAAUU'
+  assert to_rna('ACGTGGTCTTAA') or { 'Invalid DNA: ' + err.msg } == 'UGCACCAGAAUU'
 }
 
 fn test_it_transcribes_all_dna_nucleotides_to_rna_equivalents_including_invalid_dna_nucleotides() {
